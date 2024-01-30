38d47f384625fe1d6fab9c43c0389d454ab180d0
eslansa <eslansanchez12345@gmail.com>
2024-01-29 22:41:28 -0500

    img_xd

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
-import { EnvironmentPlugin } from 'webpack';
+import pkg from 'webpack';
+const { EnvironmentPlugin } = pkg;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: [
+            "cdn.sanity.io"
+        ],
         remotePatterns: [
             {
                 protocol: "https",
