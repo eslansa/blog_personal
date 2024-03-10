dbbe03844912e64f865594a0101d3e734b2dff93
eslansa <eslansanchez12345@gmail.com>
2024-03-10 13:30:41 -0500

    verific_google

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
         <Provider>
           <Head>
             <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+            <meta name="google-site-verification" content="7pY3HSqGDEy_LyhtYnPQmWomQGWQkC740Gmb9Qk6_FI" />
           </Head>
           <Navbar />
           <main className="mx-auto max-w-5xl px-6">
