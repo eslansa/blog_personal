e1b003b67f6bc94c7484a169381e0c730b9bfffc
eslansa <eslansanchez12345@gmail.com>
2024-03-10 02:16:57 -0500

    change_load

--- src/app/(client)/loading.tsx
+++ src/app/(client)/loading.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
+import SkeletonLoader from "@/components/SkletonLoading";
 
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return (
-    <div className="w-100 h-100 rounded-sm overflow-hidden mb-4 relative">
-      {/* <Image src="/loading.png" alt="loading" layout="fill" objectFit="cover" /> */}
-      <p>Loading...</p>
-    </div>
-  );
+ return (
+    <SkeletonLoader />
+ );
 }
