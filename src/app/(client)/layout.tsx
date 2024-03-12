56a2293ba440e97dde3c5c1faf39cc3a14d5af21
eslansa <eslansanchez12345@gmail.com>
2024-03-11 20:16:10 -0500

    SEO

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -10,13 +10,49 @@ export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 const inter = Inter({ subsets: ["latin"] });
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "eslansaBlog",
-  description: "Blog de Eslán Sánchez Alvarez, artículos, información y más sobre el mundo web.",
-  verification: {
-    google: '7pY3HSqGDEy_LyhtYnPQmWomQGWQkC740Gmb9Qk6_FI',
+export const metadata : Metadata = {
+  generator: 'Next.js',
+  applicationName: 'Eslán | Blog',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Eslán', 'eslansa', 'Blog', 'web', 'Sánchez', 'dev'],
+  authors: [{ name: 'Eslán' }, { name: 'Sánchez', url: 'https://eslan.pages.dev' }],
+  creator: 'Eslán Sánchez',
+  publisher: 'Eslan Sanchez',
+  formatDetection: {
+     email: true,
+     address: false,
+     telephone: false,
   },
-};
+  title: {
+     template: 'Eslán | Blog',
+     default: 'Eslan Sanchez', // a default is required when creating a template
+  },
+  openGraph: {
+     title: 'Eslán | Blog',
+     description: "Eslán Sánchez Alvarez descubre el blog de , donde encontrarás artículos, información y más sobre el mundo web.",
+     url: 'https://eslan.pages.dev',
+     siteName: 'Eslan Sanchez',
+     images: [
+      {
+        url: 'https://eslan.pages.dev/_next/image?url=%2Favatar.webp&w=1920&q=70', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://eslan.pages.dev/_next/image?url=%2Favatar.webp&w=1920&q=70', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'Avatar',
+      },
+    ],
+    locale: 'es_ES',
+    type: 'website',
+    },
+  verification: {
+     google: '7pY3HSqGDEy_LyhtYnPQmWomQGWQkC740Gmb9Qk6_FI',
+  },
+ };
+ 
 
 export default function RootLayout({
   children,
