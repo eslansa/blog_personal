6c7d3464310ff5feb93d29c5155573c246c326c1
eslansa <eslansanchez12345@gmail.com>
2024-03-17 23:47:16 -0500

    sitemap and robot ts

---
+++ src/app/robots.ts
@@ -0,0 +1,20 @@
+// robots.tsx
+
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: '/private/',
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot'],
+        disallow: '/',
+      },
+    ],
+    sitemap: 'https://eslan.pages.dev/sitemap.xml',
+  }
+}
